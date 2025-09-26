e767ab74c230466bbbf5dd569915312136409922
xianlonluohan <2047669296@qq.com>
2025-09-26 22:59:02 +0800

    优化示例

--- main.ts
+++ main.ts
@@ -68,6 +68,7 @@ namespace emakefun {
     //% baud_rate.defl=BaudRate.BaudRate9600
     //% weight=100
     export function initEspAtModule(): void {
+        serial.readBuffer(0);
         restart(2000);
         const at_commands = [
             "ATE0",
